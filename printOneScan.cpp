e71c3b7e325a17d5f8ef7fb853186b94a59c1e28
glafratta <g.lafratta@outlook.com>
2023-03-06 12:41:30 +0000

    allow lidar to warm up

--- printOneScan.cpp
+++ printOneScan.cpp
@@ -1,8 +1,10 @@
 #include "a1lidarrpi.h"
+#include <fstream>
 
 class DataInterface : public A1Lidar::DataInterface {
 public:
 	FILE * f;
+//	std::ofstream o;
 	int counter=0;
     char fileName[256];
 
@@ -14,21 +16,27 @@ public:
 		y=ltm->tm_year-100;
 		m = ltm->tm_mon +1;
 		d=ltm->tm_mday;
-		sprintf(fileName, "scan%02i%02i%02i_%02i%02i", d,m,y, ltm->tm_hour, ltm->tm_min);
+		sprintf(fileName, "/tmp/scan%02i%02i%02i_%02i%02i.txt", d,m,y, ltm->tm_hour, ltm->tm_min);
+	//	o = std::ofstream(fileName);
 		f=fopen(fileName, "w+");
 		fclose(f);
 
 	}
-
+	
 	void newScanAvail(float,A1LidarData (&data)[A1Lidar::nDistance]) {
-		f=fopen(fileName, "a");
-		for(A1LidarData &data: data) {
-			if (data.valid)
-				fprintf(f, "%.2f\t%.2f\n",
-				       data.x,
-				       data.y);
-		}
 		counter++;
+		f=fopen(fileName, "a+");
+		printf("writing to %s\n", fileName);
+		for(A1LidarData &data: data) {
+			if (data.valid){
+			fprintf(f,"%.2f\t%.2f\n",data.x,data.y);
+		//		o<<data.x<<"\t"<<data.y<<"\n"<<std::flush;
+		}
+			//else{
+			//printf("invalid data");
+			//}
+		}
+
 		fclose(f);
 	}
 };
@@ -41,7 +49,9 @@ int main(int, char **) {
 	lidar.registerInterface(&dataInterface);
 	lidar.start();
 	do {
-	} while (dataInterface.counter<1);
+}while (std::ifstream(dataInterface.fileName).peek()==std::ifstream::traits_type::eof());
+	//dataInterface.o<<std::flush;
+	//dataInterface.o.close();
 	lidar.stop();
 	printf("done\n");
 
