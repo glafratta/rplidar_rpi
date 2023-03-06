13f54cc44006e0b5832e56803af47e7340915044
glafratta <g.lafratta@outlook.com>
2023-03-06 11:00:46 +0000

    fixed printonescan

--- printOneScan.cpp
+++ printOneScan.cpp
@@ -6,37 +6,38 @@ public:
 	int counter=0;
     char fileName[256];
 
-	DataInterface(){
-	// //TODAYS DATE AND TIME
-    time_t now =time(0);
-    tm *ltm = localtime(&now);
-    int y,m,d;
-    y=ltm->tm_year-100;
-    m = ltm->tm_mon +1;
-    d=ltm->tm_mday;
-    sprintf(fileName, "scan%02i%02i%02i_%02i%02i", d,m,y, ltm->tm_hour, ltm->tm_min);
-	f=fopen(fileName, "w+");
-	fclose(fileName);
+	void __init__(){
+		// //TODAYS DATE AND TIME
+		time_t now =time(0);
+		tm *ltm = localtime(&now);
+		int y,m,d;
+		y=ltm->tm_year-100;
+		m = ltm->tm_mon +1;
+		d=ltm->tm_mday;
+		sprintf(fileName, "scan%02i%02i%02i_%02i%02i", d,m,y, ltm->tm_hour, ltm->tm_min);
+		f=fopen(fileName, "w+");
+		fclose(f);
 
 	}
+
 	void newScanAvail(float,A1LidarData (&data)[A1Lidar::nDistance]) {
 		f=fopen(fileName, "a");
 		for(A1LidarData &data: data) {
 			if (data.valid)
 				fprintf(f, "%.2f\t%.2f\n",
 				       data.x,
-				       data.y,
-);
+				       data.y);
 		}
 		counter++;
-		fclose(fileName);
+		fclose(f);
 	}
 };
 
 int main(int, char **) {
 	fprintf(stderr,"prints x, y coordinates approximated to the second decimal\n");
 	A1Lidar lidar;
-	DataInterface dataInterface();
+	DataInterface dataInterface;
+	dataInterface.__init__();
 	lidar.registerInterface(&dataInterface);
 	lidar.start();
 	do {
